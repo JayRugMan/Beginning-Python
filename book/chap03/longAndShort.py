e21f8fce6dfa6f596e23d239e5b1ffa56679148c
JayRugMan <jasonrhardman@gmail.com>
2019-07-30 12:40:48 -0600

    Added some comments and print line for "point" to longAndShort

--- book/chap03/longAndShort.py
+++ book/chap03/longAndShort.py
@@ -5,16 +5,21 @@ import time
 
 
 def theLongAndShort(mystring):
+    # Expands string by lengthening the space the string is joined by
     for spaces in range(12):
         print('{}'.format(' ' * spaces).join(list(mystring)))
         time.sleep(0.5)
+    # Makes the "point" of the "arrowhead" by expanding by 12 spaces
+    print('{}'.format(' ' * 12).join(list(mystring)))
+    time.sleep(0.5)
+    # contracts string by shortening the space the string is joined by
     for spaces in range(11, -1, -1):
         print('{}'.format(' ' * spaces).join(list(mystring)))
         time.sleep(0.5)
 
 
 def main():
-    userInput = input('Enter a string to stretch: ')
+    userInput = input('Enter a string to stretch (at least two characters): ')
     theLongAndShort(userInput)
 
 
