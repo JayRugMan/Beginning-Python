b90799666e01c3cd86e3b73ffb22f2b334850c7b
JayRugMan <jasonrhardman@gmail.com>
2019-08-02 16:17:33 -0600

    Added file simpleDatabase-2.0.py to chap04

---
+++ book/chap04/simpleDatabase-2.0.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python3.7
+# Another simple database, but this one uses the get() method
+
+# A dictionary of People and their info
+people = {
+
+    'Alice': {
+        'phone': '2341',
+        'addr': 'Foo Drive 23'
+    },
+
+    'Beth': {
+        'phone': '9102',
+        'addr': 'Bar Street 42'
+    },
+
+    'Cecil': {
+        'phone': '3158',
+        'addr': 'Baz Avenue 90'
+    },
+
+    'Jason': {
+        'phone': '255',
+        'addr': 'Westbank Street'
+    }
+
+}
+
+# A dictionary to translate coded labels to spoken English
+labels = {
+    'phone': 'phone extension',
+    'addr': 'address'
+}
+
+# Get the name
+name = input('Name: ')
+
+# Are we looking for the address or extension
+request = input('Phone extenstion (e) or address (a)? ')
+
+# Correct-key check
+key = request  # In case the request is neither 'p' or 'a'
+if request == 'p':
+    key = 'phone extension'
+if request == 'a':
+    key = 'address'
+
+# Using get to provide default values
+person = people.get(name, {})
+label = labels.get(key, key)
+result = person.get(key, 'not available')
+
+print("{}'s {} is {}".format_map(name, label, result))
