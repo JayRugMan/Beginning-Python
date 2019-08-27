88d3c54ccdd4d6bf14b9ebcfad75fb67f3d7b2d2
JayRugMan <jasonrhardman@gmail.com>
2019-08-27 16:51:45 -0600

    Added fibs.py

--- book/chap06/fibs.py
+++ book/chap06/fibs.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 
-fibs = [0, 1]
+# fibs = [0, 1]
+# num = int(input('How many Fibonacci numbers do you want? '))
+# for i in range(num-2):
+#     fibs.append(fibs[-2] + fibs[-1])
+# [print(nm) for nm in fibs]
+
+
+def fibSeq(num):
+    fibs = [0, 1]
+    for i in range(num-2):
+        fibs.append(fibs[-2] + fibs[-1])
+
+    return fibs
+
+
 num = int(input('How many Fibonacci numbers do you want? '))
-for i in range(num-2):
-    fibs.append(fibs[-2] + fibs[-1])
-[print(nm) for nm in fibs]
+print(fibs(num))
