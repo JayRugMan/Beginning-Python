41d12a3bd40931b912731b5f796552ce7dc43fae
JayRugMan <jasonrhardman@gmail.com>
2019-09-09 15:34:29 -0600

    Drastic overhaul of output formatting and format coding, made closing if statment into dictionary instead, created actions and options dictionaries for dynamic 'f' option

--- myOwn/thisOldHouse.py
+++ myOwn/thisOldHouse.py
@@ -34,7 +34,7 @@ def random_password():
     return pw
 
 
-def build_house(password):
+def build_house():
     """Creates the house dictionary, a dictionary on how the rooms connect,
     and a compass dictionary defining directions right or left of oneanother"""
 
@@ -51,7 +51,7 @@ def build_house(password):
             'west': 'a wall'},
         'study': {
             'north': 'an open door',
-            'east': password,
+            'east': random_password(),
             'south': 'a wall',
             'west': 'a wall'},
         'master bedroom': {
@@ -99,46 +99,69 @@ def build_house(password):
             'south': 'west',
             'west': 'north'}
     }
-    return house, infrastructure, compass
+    options = {  # used for dynamic heads up display options menu
+        'q': ' quit',
+        'f': ' move forward',
+        'r': ' turn right',
+        'l': ' turn left'
+    }
+    actions = {  # used to update options dictinary per what's ahead
+        'a locked door': 'unlock door',
+        'a map': 'look at map',
+        'the exit': 'exit',
+        'an open door': 'walk through',
+        'a wall': 'move forward',
+        'a closed door': 'try door',
+        house['study']['east']: 'move forward'
+    }
+    return house, infrastructure, compass, options, actions
 
 
 def define_player(infrastructure, compass):
     """Sets initial room and direction randomly"""
 
     directions = [key for key in compass['r']]
-    rooms = [r for r in infrastructure]
+    rooms = [rm for rm in infrastructure]
     room = rooms[random.randint(0, 4)]  # does not include locked room
     direction = directions[random.randint(0, 3)]
     player = {
         'room': room,
         'facing': direction,
-        'won': False
+        'won': 'no'
     }
     return player
 
 
-def hud(house, player):
+def hud(house, player, options, actions):
     """Prints HUD with which room your in, which direction you're facing, What
     is straight ahead, and what the feedback is from the host of the house"""
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')  # clear screen
     ahead = house[player['room']][player['facing']]
+    # Hackish, for now - if pw is updated, new actions key/value needed
+    actions[house['study']['east']] = 'move forward'
+    # Option for f changed depending on what's ahead
+    options['f'] = actions[ahead]
     feedback = house['feedback']
-    print("""=== This Old House ===
-
-You are in the {room} facing {facing}
-
-straight ahead is {hd}
-
-== Options ==
-l = turn left
-r = turn right
-f = move forward
-q = quit
-
-"{fb}"
-"What would you like to do?"
-""".format(**player, hd=ahead, fb=feedback))
+    output_list = [
+        '=== This Old House ===',
+        '',
+        'You are in the {room} facing {facing}'.format(**player),
+        '',
+        'straight ahead is {}'.format(ahead),
+        '',
+        '===== Options =====',  # options will be inserted below this line
+        '',
+        '"{}"'.format(feedback),
+        '"What would you like to do?"',
+        ''
+    ]
+    opt_tbl = '{0:<2}{1:.>17}'  # makes options box 17 wide in justified format
+    # inserts justified table into output list
+    for input, action in options.items():
+        output_list.insert(7, opt_tbl.format(input, action))
+    for line in output_list:  # Prints each line centered from output_list
+        print('{0:^57}'.format(line))
     return
 
 
@@ -162,8 +185,8 @@ T |                |                |                 | T
 
                         -SOUTH-
 """)
-
-    input("\t      hit ENTER once you're done")
+    prompt = 'Hit ENTER to close map.'
+    input('{:^57}'.format(prompt))
     return
 
 
@@ -189,14 +212,14 @@ def forward(house, player, infrastructure):
         # your direction also changes to south
         if player['room'] == 'bathroom':
             house['feedback'] = 'You get the treasure, now leave in peace.'
-            player['won'] = True
+            player['won'] = 'yes'
             player['facing'] = 'south'
             house['foyer']['west'] = 'a closed door'
-        # if not in room five, feedback is
+        # if not in the bathroom, feedback is
         # given that you are in a new room
         else:
             house['feedback'] = "You're in a new room"
-    # if the door is locked and you move forward,
+    # if the door is locked and you choose to unlock it,
     # you are challenged for a password
     elif ahead == 'a locked door':
         # if you get the password right, the
@@ -242,25 +265,27 @@ def player_interactions(house, player, infrastructure, compass):
 def main():
     'Main Function'
 
-    # Set random password
-    password = random_password()
-    # Build House and include password on one of the walls
-    house, infrastructure, compass = build_house(password)
+    # Build environmental dictionaries necessary for gameplay
+    house, infrastructure, compass, options, actions = build_house()
     # Initialize player with start room, direction, and "won" status
     player = define_player(infrastructure, compass)
+    # Defines formatting and phrases for exiting comments
+    centered_output = '\n{:^57}\n'
+    closing_output = {
+        'yes': 'Congradulations!! Spend it on charity, will ya?!',
+        'no': 'Smell ya later!'
+    }
     # Enter gameplay loop
     while True:
         # Heads Up Display
-        hud(house, player)
+        hud(house, player, options, actions)
         # Get player interaction
         player_interactions(house, player, infrastructure, compass)
         if player['input'] == 'q':  # quit if q
             break
     os.system('cls' if os.name == 'nt' else 'clear')
-    if player['won']:
-        print("\n\tCongradulations!!\n\tSpend it on charity, will ya?!\n")
-    else:
-        print('\n\tSmell you later!\n')
+    # If player won is yes, closing output changes
+    print(centered_output.format(closing_output[player['won']]))
 
 
 main()
