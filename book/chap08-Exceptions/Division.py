bd5a39519a64b93d07eb207b1b89ad025d17dc85
JayRugMan <jasonrhardman@gmail.com>
2020-03-24 22:12:58 -0600

    I added two scripts, Division and Division_with_Exceptions, and Python Bookmarks HTML, as well as some content to commands_08.txt

---
+++ book/chap08-Exceptions/Division.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+x = int(input('Enter the first number: '))
+y = int(input('Enter the second number: '))
+print(int(x/y))
---
+++ book/chap08-Exceptions/Division_with_Exceptions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def calculator():
+    x = int(input('Enter the first number: '))
+    y = int(input('Enter the second number: '))
+    print(int(x/y))
+
+
+try:
+    calculator()
+except ZeroDivisionError:
+    print("The second number can't be zero.")
