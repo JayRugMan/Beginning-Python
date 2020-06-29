83ddddeb24a5c1953bb78a7c15b353602f7a07e5
JayRugMan <jasonrhardman@gmail.com>
2020-06-29 08:31:13 -0600

    Modularized Box and Line_Characters in lyeCalc.py

--- myOwn/lyeCalc.py
+++ myOwn/lyeCalc.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 # lyeCalc.py
 
+from jaymatting.Box import Box
+from jaymatting.Line_Characters import Line_Characters
+
 
 class SAP_Basics:
     ''' Set constants for Saponification Values
@@ -65,21 +68,6 @@ class Final_Values():
             self.percLst[num]['water'] = waterAmnt*((100-num)*0.01)
 
 
-class Line_Characters:
-    '''Characters for box and table'''
-
-    def __init__(self):
-        self.tpBtm = '━'
-        self.cornerTL = '┏'
-        self.cornerTR = '┓'
-        self.cornerBR = '┛'
-        self.cornerBL = '┗'
-        self.edges = '┃'
-        self.tblCross = '┼'
-        self.tblTop = '─'
-        self.tblEdges = '│'
-
-
 class Table(Line_Characters):
     '''Builds table object'''
 
@@ -125,52 +113,6 @@ class OutPut:
           '(recommended about 5 - 8%)\n'
         )
         self.outputDict['center3'] = table.build_table()
-
-
-class Box(Line_Characters):
-    '''Construct a box around the input'''
-
-    def __init__(self):
-        super().__init__()
-
-    def get_width(self, innardsDict):
-        '''Gets the longest line of the string'''
-        longest = 0
-        for value in innardsDict.values():
-            innardsLst = value.split('\n')
-            for item in innardsLst:
-                if len(item) > longest:
-                    longest = len(item)
-        return longest
-
-    def fill_box(self, innardsDict, width):
-        '''Either centers, or justifies left or right
-        values based on keys and returns a single string
-        combining each section formatted properly'''
-        strList = []
-        for key, value in innardsDict.items():
-            if 'center' in key:
-                s = '{0}{1:^{w}}{0}'
-            elif 'right' in key:
-                s = '{0}{1:>{w}}{0}'
-            elif 'left' in key:
-                s = '{0}{1:<{w}}{0}'
-            for line in value.split('\n'):
-                strList.append(s.format(self.edges, line, w=width))
-        return '\n'.join(strList)
-
-    def put_in_box(self, innardsDict):
-        '''Puts the specified string into a box recieves a dict of strings,
-        separated based on number of differing formatting - centered, right
-        or left justified, the format(s) being the key(s)'''
-        width = self.get_width(innardsDict) + 2
-        top = '{}{}{}'.format(self.cornerTL, self.tpBtm*(width), self.cornerTR)
-        bottom = '{}{}{}'.format(self.cornerBL, self.tpBtm*(width),
-                                 self.cornerBR)
-        fString = '{t}{nl}{guts}{nl}{b}'
-        boxedStr = fString.format(t=top, nl='\n', b=bottom,
-                                  guts=self.fill_box(innardsDict, width))
-        return boxedStr
 
 
 def get_input(inputType, valueType, listOfItems=[]):
