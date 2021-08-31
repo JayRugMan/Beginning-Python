9eb948f510d53b94289b8f2a93b89d3d5d945452
jasonrhardman <jason.hardman@churchofjesuschrist.org>
2021-08-31 15:00:59 -0600

    udpated global and local argument naming conventions in pdfsplice.sh

--- bash/pdfsplice.sh
+++ bash/pdfsplice.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# used pdfseparate and pdfunite to extract and combine
+# uses pdfseparate and pdfunite to extract and combine
 # pages from a PDF file
 
 
-function fusage() {
+function usage() {
   cat <<EOF
-Usage for ${scriptName}:
+Usage for ${SCRIPT_NAME}:
 
  -h                  Prints usage
 
@@ -23,13 +23,13 @@ Usage for ${scriptName}:
  -d                  Delete the original file
 
 Example:
-${scriptName} -s 3 -e 14 -f myfile.pdf -f myfile_3-14.pdf
+${SCRIPT_NAME} -s 3 -e 14 -f myfile.pdf -f myfile_3-14.pdf
 
 EOF
 }
 
 
-function testInt() {
+function test_int() {
   # Tests whether input is integer
   re='^[0-9]+$'
   if [[ ${1} =~ $re ]]; then
@@ -40,112 +40,113 @@ function testInt() {
 }
 
 
-function interactiveMode() {
+function interactive_mode() {
   # Interactive Mode
-  read -p "Input File Name: " -e iFile
-  read -p "Output File Name: " -e oFile
-  read -p "First Page (leave blank if page 1): " -e firstPage
-  if [[ -z $firstPage ]]; then
-    firstPage=1;
-  elif ! testInt $firstPage; then
-    fusage
+  read -p "Input File Name: " -e INPUT_FILE
+  read -p "Output File Name: " -e OUTPUT_FILE
+  read -p "First Page (leave blank if page 1): " -e FIRST_PAGE
+  if [[ -z $FIRST_PAGE ]]; then
+    FIRST_PAGE=1;
+  elif ! test_int $FIRST_PAGE; then
+    usage
     exit
   fi
-  read -p "Last Page (leave blank if last page): " -e lastPage
-  if [[ -z $lastPage ]]; then
-    lastPage=$(pdfinfo $iFile | awk '/Pages:/ {print $2}');
-  elif ! testInt $lastPage; then
-    fusage
+  read -p "Last Page (leave blank if last page): " -e LAST_PAGE
+  if [[ -z $LAST_PAGE ]]; then
+    LAST_PAGE=$(pdfinfo $INPUT_FILE | awk '/Pages:/ {print $2}');
+  elif ! test_int $LAST_PAGE; then
+    usage
     exit
   fi
   while true; do
-    read -p "Do you want to delete the original file ('y' or 'n'): " -e delBit
-    if [[ $delBit == "y" ]] || [[ $delBit == "n" ]]; then
-      if [[ $delBit == "y" ]]; then
-        deleteOriginal=true
-        break
+    read -p "Do you want to delete the original file ('y' or 'n'): " -e del_bit
+    if [[ $del_bit == "y" ]] || [[ $del_bit == "n" ]]; then
+      if [[ $del_bit == "y" ]]; then
+        DEL_ORIGINAL=true
       fi
+      break
     fi
   done
 }
 
 
-function getOptions() {
-  local OPTIND OPTION h i s e f d
-  while getopts "his:e:f:d" OPTION; do
-    case "$OPTION" in
+function get_options() {
+  local OPTIND the_opt h i s e f d
+  while getopts "his:e:f:d" the_opt; do
+    case "$the_opt" in
       h) # prints help/usage
-        fusage
-	exit 0
+        usage
+	    exit 0
         ;;
       i) # Interactive Mode
-        interactiveMode
+        interactive_mode
+		break
         ;;
       s) # first page
-        firstPage="$OPTARG"
-        if ! testInt $firstPage; then fusage; exit; fi
+        FIRST_PAGE="$OPTARG"
+        if ! test_int $FIRST_PAGE; then usage; exit; fi
         ;;
       e) # last page
-        lastPage="$OPTARG"
-	      if ! testInt $lastPage; then fusage; exit; fi
+        LAST_PAGE="$OPTARG"
+	    if ! test_int $LAST_PAGE; then usage; exit; fi
         ;;
       f) # takes two arguments, input file and output file
-	      Files+=("${OPTARG}")
+	    the_files+=("${OPTARG}")
         ;;
       d) # Delete if designated
-        deleteOriginal=true
+        DEL_ORIGINAL=true
         ;;
       ?)
-        fusage
+        usage
         exit 1
         ;;
     esac
   done
-  shift "$((OPTIND -1))"
-  if [[ ! -z $Files ]]; then
-    iFile=${Files[0]}
-    oFile=${Files[1]}
+  shift "$((OPTIND-1))"
+  if [[ ! -z $the_files ]]; then
+    INPUT_FILE=${the_files[0]}
+    OUTPUT_FILE=${the_files[1]}
   fi
-  if [[ $oFile == "" ]]; then oFile=PDF_OutputFile.pdf; fi
-  if [[ $lastPage -eq 0 ]]; then lastPage=$(pdfinfo "${iFile}" | awk '/Pages:/ {print $2}'); fi
+  if [[ $OUTPUT_FILE == "" ]]; then OUTPUT_FILE=PDF_OutputFile.pdf; fi
+  if [[ $LAST_PAGE -eq 0 ]]; then LAST_PAGE=$(pdfinfo "${INPUT_FILE}" | awk '/Pages:/ {print $2}'); fi
 }
 
 
-function parseFile() {
+function parse_file() {
   # Where the magic happens
-  ##JH numOfPages=$(( $((lastPage + 1)) - firstPage))
+  ##JH numOfPages=$(( $((LAST_PAGE + 1)) - FIRST_PAGE))
   # separate pdf into temp files from input file
-  pdfseparate -f ${firstPage} -l ${lastPage} "${iFile}" tempFile-%d.pdf
-  tmpFileArray=()
+  pdfseparate -f ${FIRST_PAGE} -l ${LAST_PAGE} "${INPUT_FILE}" tempFile-%d.pdf
+  tmp_file_array=()
   # Create an array with file names of temp
   ##JH for i in $(seq 1 $numOfPages); do
-  for i in $(seq ${firstPage} ${lastPage}); do
-    tmpFileArray+=(tempFile-${i}.pdf)
+  for i in $(seq ${FIRST_PAGE} ${LAST_PAGE}); do
+    tmp_file_array+=(tempFile-${i}.pdf)
   done
   # Unite temp files, then delete them
-  pdfunite ${tmpFileArray[@]} "${oFile}"
+  pdfunite ${tmp_file_array[@]} "${OUTPUT_FILE}"
   rm -f tempFile-*.pdf
   # Delete original if designated
-  if ${deleteOriginal} ; then
-    rm -f ${iFile}
+  if ${DEL_ORIGINAL} ; then
+    rm -f ${INPUT_FILE}
   fi
 }
 
 
 function main() {
   # Main Function
-  getOptions ${@}
-  parseFile
+  get_options ${@}
+  parse_file
 }
 
 #### Declare GLOBAL arguments ####
 
-scriptName=${0}
-iFile=""
-oFile=""
-firstPage=1
-lastPage=0
-deleteOriginal=false
+SCRIPT_NAME=${0}
+INPUT_FILE=""
+OUTPUT_FILE=""
+FIRST_PAGE=1
+LAST_PAGE=0
+DEL_ORIGINAL=false
 
 #### Execute main function ####
 
