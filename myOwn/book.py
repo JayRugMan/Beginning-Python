546d7f5219dac65e19ade3044b60aa17ae8150c8
jayrugman <jasonrhardman@gmail.com>
2022-03-16 01:34:41 -0600

    Now book.py is functional in coding messages. Next is getting it to decode

--- myOwn/book.py
+++ myOwn/book.py
@@ -5,13 +5,17 @@ import base64
 import sys
 import argparse
 import secrets
+from os.path import exists
 
 
-def main():
-    the_description='Using a file as a "book," a message can be processed into a code or vise-versa.'
+def get_args(argv=None):
+    '''Uses argparse to get input'''
+
+    the_description='Using a file as a "book," a message can ' \
+        'be processed into a code or vise-versa.'
     help_help = '''\
                  You get this helpful message before exiting
- 
+
 '''
     book_help = '''\
                  This designates the binary file that acts as the
@@ -20,30 +24,30 @@ def main():
                  Keep in mind if the file is too small, the entropy
                  will be small; if the file is too big, the process
                  time and performance may be impacted.
- 
+
 '''
     message_help='''\
                  This is the message string to be encoded. It can be
                  a string or a plain-text file. This cannot be used
                  with -c/--code.
- 
+
 '''
     code_help='''\
                  The code goes here. This cannot be used with
                  -m/--message.
- 
+
 '''
     the_epilog='''\
 
   Special Characters -                   Only the following special characters will work:
                                            ---  . , ' ? + - = : ! @ # $ % ^ & * /  ---
-                                         
+
                                          Note that some special characters will encode better
                                          if a message file is used instead of a string on the
                                          terminal
- 
+
 '''
-        
+
     parser = (argparse.ArgumentParser(
                   prog='book.py',
                   formatter_class=lambda prog: argparse.HelpFormatter(
@@ -76,82 +80,109 @@ def main():
                         type=str,
                         help=code_help
                        )
-    
-    args = vars(parser.parse_args())
-    ##JHimport getopt
-    ##JH
-    ##JH
-    ##JHdef usage(error_str=None):
-    ##JH    '''Prints usage...'''
-    ##JH##JH https://www.programcreek.com/python/example/202/getopt.GetoptError
-    ##JH    if error_str is not None:
-    ##JH        print('ERROR: {}'.format(error_str))
-    ##JH
-    ##JH    print('''
-    ##JH
-    ##JHUsage:
-    ##JH
-    ##JH-b <binary file>,
-    ##JH--book=<binary file>       This designates the binary file that acts as the
-    ##JH                           "book" in the book code and can be any type of
-    ##JH                           binary file, like a webpage, img, .jpg, .mp4, etc.
-    ##JH                           Keep in mind if the file is too small, the entropy
-    ##JH                           will be small; if the file is too big, the process
-    ##JH                           time and performance may be impacted.
-    ##JH
-    ##JH-m "string",
-    ##JH--message="string"         This is the message string to be encoded. It can be
-    ##JH                           a string or a plain-text file. This cannot be used
-    ##JH                           with -c/--code.
-    ##JH
-    ##JH-c <numbers>,
-    ##JH--code=<numbers>           The code goes here. This cannot be used with
-    ##JH                           -m/--message.
-    ##JH
-    ##JH*                          Prints this helpful output
-    ##JH
-    ##JH
-    ##JHSpecial Characters         Only the following special characters will work:
-    ##JH                                  . , ' ? + - = : ! @ # $ % ^ & * /
-    ##JH
-    ##JH                           Note that some special characterswill encode better
-    ##JH                           if a message file is used instead of a string on the
-    ##JH                           terminal
-    ##JH
-    ##JH    ''')
-    ##JH
-    ##JH
-    ##JHdef main(argv):
-    ##JH    '''Main event'''
-    ##JH
-    ##JH    try:
-    ##JH        opts, arg = getopt.getopt(argv, "b:m:c:",["book=","message=","code="])
-    ##JH    except getopt.GetoptError:
-    ##JH        usage()
-    ##JH        sys.exit(2)
-    ##JH
-    ##JH    for opt, arg in opts:
-    ##JH        if opt in ('-b', '--book'):
-    ##JH            book_file = arg
-    ##JH            print(book_file)
-    ##JH        elif opt in ('-m', '--message'):
-    ##JH            message = arg
-    ##JH            print(message)
-    ##JH        elif opt in ('-c', '--code'):
-    ##JH            code = arg
-    ##JH            print(code)
-    
-    with open(args['book_file'], 'rb') as the_book:
+
+    return vars(parser.parse_args(argv))
+
+
+def process_message_file(message_file):
+    '''turns file into a string for further processing'''
+
+    ex_msg = "doesn't look like a message is in " + \
+        message_file + \
+        "Try one with ascii text next time."
+
+    try:
+        with open(message_file, 'r') as file:
+            file_lst = [i for i in file.read().split('\n') if len(i) != 0]
+    except UnicodeDecodeError:
+        # if this is a non-text file, like an image, etc
+        print(ex_msg)
+        sys.exit(1)
+
+    return ' '.join(file_lst)
+
+
+def process_message(raw_message, specials):
+    '''Processes the message to determine if it's a sting or file, then if
+    it has any special characters, which are translated to special-character
+    strings so the can be encoded with base64 characters'''
+
+    # Returns true or false depending on whether the string provided is a
+    # file. Even if the user is trying to specify a file, but does not exist,
+    # it will treat it as a string. Harsh, I know.
+    if exists(raw_message):
+        raw_message = process_message_file(raw_message)
+
+    final_message = ''
+    for char in raw_message:
+        if char in specials.keys():
+            final_message += specials[char]
+        else:
+            final_message += char
+
+    return final_message
+
+
+def encode_message(book_f, message):
+    '''takes the book and the message and works some maigic'''
+
+    with open(book_f, 'rb') as the_book:
         book_lst = [
-          i for i in base64.encodebytes(
-            the_book.read()
+            i for i in base64.encodebytes(
+                the_book.read()
             ).decode('utf-8').split('\n') if len(i) != 0
         ]
-    book_str = '\n'.join(book_lst)
+
     num_of_lines = len(book_lst)
     book_lst.insert(0, 'These bits are the book')
-    
-    print('file used as book: {book_file}\nmessage: {the_message}'.format(**args))
+    code = ''
+    for character in message:
+        while True:
+            random_line_num = secrets.randbelow(num_of_lines)
+            if character in book_lst[random_line_num]:
+                line = ' {}'.format(book_lst[random_line_num])  # offset index
+                idx = line.index(character)
+                code += '{} {} '.format(random_line_num, idx)
+                break
+
+    return code[:-1]  # because the last char is a space
+
+
+def main():
+    '''The Main Event'''
+
+    options = None
+    special_chars = {
+        '.': "XSPp",
+        ',': "XSPc",
+        "'": "XSPa",
+        '?': "XSPq",
+        '+': "XSPl",
+        '-': "XSPd",
+        '=': "XSPe",
+        ':': "XSPo",
+        '!': "XSP1",
+        '@': "XSP2",
+        '#': "XSP3",
+        '$': "XSP4",
+        '%': "XSP5",
+        '^': "XSP6",
+        '&': "XSP7",
+        '*': "XSP8",
+        ' ': "+"
+    }
+
+    args = get_args(options)
+
+    if args['the_message']:
+        message_string = process_message(args['the_message'], special_chars)
+        final_answer = encode_message(args['book_file'], message_string)
+    elif args['the_code']:
+        final_answer = "coming soon"
+    else:
+        final_answer = 'Oppsy... how did this happen?'
+
+    print(final_answer)
 
 
 if __name__ == "__main__":
