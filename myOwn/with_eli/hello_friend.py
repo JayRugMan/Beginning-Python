2447bcbd8aa10dde75c8237c34c808e280886c58
jayrugman <jasonrhardman@gmail.com>
2023-08-17 23:15:40 -0600

    decision tree added, as well as color list

--- myOwn/with_eli/hello_friend.py
+++ myOwn/with_eli/hello_friend.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
 
-name = input("Hi, what is your name? ")
+name,color = input("Hi, what is your name and fav color? ").split()
 h = 'Hello'
-hf = h + ', ' + name + '!'
+hf = f'{h}, {name}!'
+
+color_list = [
+    'blue',
+    'gold',
+    'black',
+    'white',
+    'purple',
+    'pink',
+    'red',
+    'yellow',
+    'green',
+    'orange',
+    'gray',
+    'grey',
+    'brown'
+]
+
+if color == 'blue':
+    fc = f'{color} is my favorite color, too!'
+elif color in color_list:
+    fc = f"Cool, I guess {color} isn't bad."
+else:
+    fc = f"Hmm. I didn't know {color} was a color."
 
 print(hf)     # "Hello, <name>!"
+print(fc)
