178c2846a21570c6f53976ab8c97497078b99155
jayrugman <jasonrhardman@gmail.com>
2024-01-30 14:13:04 -0700

    added elements for color

--- myOwn/with_eli/math.py
+++ myOwn/with_eli/math.py
@@ -3,6 +3,26 @@
 import os
 import random
 import operator
+try:
+    from colorama import init as c_init
+    from colorama import Fore, Style
+except ModuleNotFoundError:
+    print(
+        '''Import Error: You don't have colorama installed.\n'''
+        '''run: pip3 install colorama'''
+    )
+
+
+# Sometimes the world needs color
+c_init()
+global c_bl; c_bl = Fore.BLUE       # cyan 
+global c_cy; c_cy = Fore.CYAN       # cyan 
+global c_gr; c_gr = Fore.GREEN      # green 
+global c_rd; c_rd = Fore.RED        # red 
+global c_yl; c_yl = Fore.YELLOW     # yellow 
+global c_dm; c_dm = Style.DIM       # dim 
+global c_br; c_br = Style.BRIGHT    # bright 
+global c_r;  c_r = Style.RESET_ALL  # reset color - needed to terminate color
 
 
 def get_operands(oper):
@@ -31,7 +51,7 @@ def get_operands(oper):
             if a % b == 0 and a != b and (a/b) <= 12 and b <= 12:
                 break
     else:
-        raise ValueError("Something went sideways with get_operands")
+        raise ValueError(f"{c_rd}Something went sideways with get_operands{c_r}")
     
     return a, b
 
@@ -55,9 +75,9 @@ def do_math(op):
         try:
             possible = int(input("How many problems to you want to do (between 1 and 100): "))
             if possible > 100:
-                print(f"😳 {possible} is too many...")
+                print(f"😳 {c_yl}{possible} is too many...{c_r}")
             elif possible <= 0:
-                print(f"😉 Uh...!? it's got to be more than 0")
+                print(f"😉 {c_br}{c_bl}Uh...!? it's got to be more than 0{c_r}")
             else:
                 break
         except ValueError:
@@ -109,11 +129,10 @@ def print_menu():
         'e/q/x': "Exit"
     }
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(f"===== Welcome to Math Game ======")
+    print("===== Welcome to Math Game ======")
     print("\n-- Choose an operation --\n")
     for key,value in opts.items():
         print(f" {key:5} - {value}")
-
 
 
 def main():
